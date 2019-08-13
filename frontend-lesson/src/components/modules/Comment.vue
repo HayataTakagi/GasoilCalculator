bb829c7eccfcc81863de4965a4a1a4ea24b8cb31
pei0804 <peeeei0804@gmail.com>
2019-08-14 01:37:45 +0900

    initial commit (+3 squashed commits) Squashed commits: [c3330ce] u [0eac90d] importきったね [b41c030] せい

---
+++ frontend-lesson/src/components/modules/Comment.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-card
+            class="mx-auto"
+            color="#26c6da"
+            dark
+            max-width="400"
+    >
+        <v-card-title>
+            <v-icon
+                    large
+                    left
+            >
+                mdi-comment
+            </v-icon>
+            <span class="title font-weight-light">Comment</span>
+        </v-card-title>
+
+        <v-card-text class="headline font-weight-bold">
+            ここにコメントが表示されます
+        </v-card-text>
+        <v-card-actions>
+            <v-list-item class="grow">
+                <v-list-item-avatar color="grey darken-3">
+                    <v-img
+                            class="elevation-6"
+                            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+                    ></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                    <v-list-item-title>Evan You</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-card-actions>
+    </v-card>
+</template>
