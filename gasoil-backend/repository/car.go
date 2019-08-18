125ede739b9c48acd25dfe113cd1da30ea4671bc
高木颯汰 <kozamurai882@gmail.com>
2019-08-19 02:43:32 +0900

    DBを追加 / 燃費データをDBに追加 / 燃費データをDBから取得

---
+++ gasoil-backend/repository/car.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/voyagegroup/treasure-app/model"
+)
+
+func AllCar(db *sqlx.DB) ([]model.Car, error) {
+	a := make([]model.Car, 0)
+	if err := db.Select(&a, `SELECT id, name, fuel_economy FROM cars`); err != nil {
+		return nil, err
+	}
+	return a, nil
+}
